4b6b7a587bdb18918295d6a7c17f0e12bf86d24f
kickbuttowski59 <pazpaz22@yahoo.com>
2017-03-03 02:40:13 +0000

    try to make scrolltop work

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( author.css )
 Rails.application.config.assets.precompile += %w( books.css )
+Rails.application.config.assets.precompile += %w( books.js )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
