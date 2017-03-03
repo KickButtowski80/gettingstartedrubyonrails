ba63a10c24ce8ac89141996d72e62ba322668520
kickbuttowski59 <pazpaz22@yahoo.com>
2017-03-03 00:33:38 +0000

    try to make scrolltop work for mobile

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -1,14 +1,18 @@
- $(document).ready(function(){
-       var $root = $('html, body');
-        $('a').click(function(e) {
-             e.preventDefault();
-             var target = this.hash;
-             var $target = $(target);
-             
+ $(document).ready(function() {
+     var $root = $('html, body');
+     $('a').click(function(e) {
+         e.preventDefault();
+         var target = this.hash;
+         var $target = $(target);
+         alert(navigator.userAgent);
+         if (navigator.userAgent.match(/(iPod|iPhone|iPad|Android)/)) {
+             window.scrollTo(200, 100) // first value for left offset, second value for top offset
+         } else {
              $("html,body").animate({
                  'scrollTop': $target.offset().top
-             }, 1000, function(){
+             }, 1000, function() {
                  window.location.hash = target;
              });
-        });
+         }
+     });
  });
